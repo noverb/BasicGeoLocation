fa9e6b1f5759aa3062836db8cedfb5a7cda08713
noverb <noverb01@hotmail.com>
2018-02-06 14:55:50 -0600

    Added geolocation Activity and Layout with basic navigation buttons - no functionality yet. Downgraded minimum version to 17 from 23

---
+++ app/src/main/java/com/naco3x/basicgeolocation/GeolocationActivity.kt
@@ -0,0 +1,25 @@
+package com.naco3x.basicgeolocation
+
+//import android.support.v7.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_geolocation.*
+
+class GeolocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geolocation)
+        geoTextView.text = "Starting out"
+        //this.textView = findViewById(R.id.geoTextView) as TextView
+
+        geoGetButton.setOnClickListener {
+                        try {
+                            geoTextView.text = "You intended to get your location! Congrats!!"
+                //startLocationUpdates()
+            }
+            catch (e : SecurityException) {
+            }
+    }
+    }
+    }
--- app/src/main/java/com/naco3x/basicgeolocation/MainActivity.kt
+++ app/src/main/java/com/naco3x/basicgeolocation/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.naco3x.basicgeolocation
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnstart.setOnClickListener {
+            val intent = Intent(this, GeolocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
